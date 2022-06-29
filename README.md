0d7cb9cc66469b186db0281fc783a364592e33ba
eyeformeta <scottreph@gmail.com>
2022-06-29 08:03:51 -0400

    {afc5fd8d-d7bc-4098-b0ad-2a6320ce0f60}

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # meta-framework
+<<<<<<< HEAD
 python meta framework 
+=======
+python meta framework
+
+~~~sh
+~.mf  [ fi -r + test - { path:bk, fo:foo bar } -out meta ] [ db -save fi-meta.db ]
+~~~
+>>>>>>> 018992b ({7be7181e-445d-4e51-be34-1a3725fc59f9})
--- mf.py
+++ mf.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+am={
+	 'app': 'meta-framework',
+	 'aliases': ['mf'],
+	 'git': 'https://raw.githubusercontent.com/eyeformeta/meta-framework/master/mf.py'
+}
 
-print('started')
+
+from sys import argv
+
+
+
+git_url_folder='https://raw.githubusercontent.com/eyeformeta/meta-framework/master/'
+git_url_file=
+print(argv)
